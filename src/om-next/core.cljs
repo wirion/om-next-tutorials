381db3ecc127585f3054d418514a975e4fddc929
François Wirion <wirion@gmail.com>
2015-12-14 15:01:26 +0100

    Change to animal example

--- src/om-next/core.cljs
+++ src/om-next/core.cljs
@@ -3,38 +3,48 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
-(defonce app-state (atom {:count 0}))
+(def app-state
+  (atom
+    {:app/title "Animals"
+     :animals/list
+     [[1 "Ant"] [2 "Antelope"] [3 "Bird"] [4 "Cat"] [5 "Dog"]
+      [6 "Lion"] [7 "Mouse"] [8 "Monkey"] [9 "Snake"] [10 "Zebra"]]}))
 
-(defui Counter
-  static om/IQuery
-  (query [this]
-    [:count])
-  Object
-  (render [this]
-          (let [count (:count (om/props this))]
-            (dom/div nil
-                     (dom/div nil (str "Count: " count))
-                     (dom/button
-                      #js {:onClick (fn [e]
-                                      (om/transact! this '[(increment)]))}
-                      "Increase")))))
+(defmulti read (fn [env key params] key))
 
-(defn read
+(defmethod read :default
   [{:keys [state] :as env} key params]
-  (let [st @state] 
-   (if-let [[_ v] (find st key)]
-      {:value v}
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      {:value value}
       {:value :not-found})))
 
-(defn mutate [{:keys [state] :as env} key params]
-  (if (= 'increment key)
-    {:value {:keys [:count]}
-     :action #(swap! state update-in [:count] inc)}
-    {:value :not-found}))
+(defmethod read :animals/list
+  [{:keys [state] :as env} key {:keys [start end]}]
+  {:value (subvec (:animals/list @state) start end)})
+
+(defui AnimalsList
+  static om/IQueryParams
+  (params [this]
+    {:start 0 :end 10})
+  static om/IQuery
+  (query [this]
+    '[:app/title (:animals/list {:start ?start :end ?end})])
+  Object
+  (render [this]
+    (let [{:keys [app/title animals/list]} (om/props this)]
+      (dom/div nil
+        (dom/h2 nil title)
+        (apply dom/ul nil
+          (map
+            (fn [[i name]]
+              (dom/li nil (str i ". " name)))
+            list))))))
 
 (def reconciler
-  (om/reconciler {:state app-state
-                  :parser (om/parser {:read read
-                                      :mutate mutate})}))
+  (om/reconciler
+    {:state app-state
+     :parser (om/parser {:read read})}))
 
-(om/add-root! reconciler Counter (gdom/getElement "app"))
+(om/add-root! reconciler
+              AnimalsList (gdom/getElement "app"))
